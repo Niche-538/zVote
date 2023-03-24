ca6d6af8832dba446b5ac3ee5c85ae1f1f138f4a
Anurag Patil <anurag.patil.1497@gmail.com>
2023-03-24 17:46:24 -0400

    Update

--- encryption.sol
+++ encryption.sol
@@ -13,18 +13,30 @@ contract MyContract {
     }
     
 
-    function fillArray() public returns(uint256[3][4][5] memory) {
+    function initialFillArray() public returns(uint256[3][4][5] memory) {
         
-        uint256 randomNum;
         for (uint256 i = 0; i < 5; i++) {
             for (uint256 j = 0; j < 4; j++) {
                 for (uint256 k = 0; k < 3; k++) {
-                    randomNum = uint256(keccak256(abi.encodePacked(i, j, block.timestamp))) % 2; // Generate a random number between 0 and 1
-                    myArray[i][j][k] = randomNum;
+                    myArray[i][j][k] = 0;
                 }
             }
         }
 
         return myArray;
     }
+
+    function fillArray() public returns(uint256[3][4][5] memory) {
+        
+        uint256 randomNum;
+        for (uint256 i = 0; i < 5; i++) {
+            for (uint256 j = 0; j < 4; j++) {
+                    randomNum = uint256(keccak256(abi.encodePacked(i, j, block.timestamp))) % 3; // Generate a random number between 0 and 1
+                    myArray[i][j][randomNum] = 1;
+            }
+        }
+
+        return myArray;
+    }
+
 }
